3dbfdc848ea8d8eafa4b59b131d1816da5e49299
hislordshipprof <proflord63@gmail.com>
2022-05-01 14:59:22 +0000

    schemas

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import { urlFor,clients } from "../../clients";
+import { urlFor,client } from "../../clients";
 import "./About.scss";
 import {images} from '../../constants'
 import { AppWrap } from "../../wrapper";
-const abouts= [
+const Abouts= [
   {title:'Web Developer', description:'I am a web developer with a passion for building web applications. I have a strong background in front-end development and have worked with a variety of technologies. I am a self-taught developer and I am always looking to learn new things.,imgUrl: "web-dev.png"', imgUrl: images.about01 },
   {title:'Frontend', description:'I am a frontend Developer and I am always looking for new opportunities to work on projects. I am also a self-taught developer and I am always looking to learn new things.',imgUrl: images.about02 },
   {title:'Backend', description:'I am a Backend Developer and I am always looking for new opportunities to work on projects. I am also a self-taught developer and I am always looking to learn new things.',imgUrl: images.about03 },
@@ -12,13 +12,13 @@ const abouts= [
 
 ]
 
-
+//urlFor
 const About = () => {
   const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
     const query='*[_type == "about"]';
-    clients.fetch(query)
+    client.fetch(query)
     .then((data) => setAbouts(data));
   },[ ])
   return (
@@ -29,7 +29,7 @@ const About = () => {
       </h2>
 
       <div className="app__profiles">
-        {abouts.map((about, index) => (
+        {Abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
@@ -37,7 +37,8 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            
+            <img src={about.imgUrl} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
