ef083b70b424a651731b91a66b820d9606a6ed95
hislordshipprof <proflord63@gmail.com>
2025-10-18 00:57:23 -0600

    modified env

--- frontend_react/src/clients.js
+++ frontend_react/src/clients.js
@@ -2,19 +2,17 @@ import sanityClient from '@sanity/client'
 import imageURLBuilder from '@sanity/image-url'
 
 // Sanity client configuration
-
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || "l3wd7476",
-  dataset: "production",
-  useCdn: false, // Disable CDN to avoid caching issues during development
-  apiVersion: "2022-02-01",
-  // Remove token completely to avoid credentials CORS issue
-  // token: process.env.REACT_APP_SANITY_TOKEN || "skC9HZmyj3pbJxiBPwv90Jo5hHgytebCk8X9bpWouQDr72z7026mfAp0WtVWkkTjMIeCha1GJHmmk0mlK4jbjpFkoXMTjvIy8BJjffkGJvqle2bwqK6oiMn7LRPCyHU0Jyq1V0CLIgaiM8Gd9aXGbPuUxs0BkxO7vTaIvaCUxCPGrKVIBwkG",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: process.env.REACT_APP_SANITY_DATASET || "production",
+  useCdn: process.env.NODE_ENV === 'production', // Use CDN in production, disable in development
+  apiVersion: process.env.REACT_APP_SANITY_API_VERSION || "2022-02-01",
+  token: process.env.REACT_APP_SANITY_TOKEN, // Use environment variable for token
+  withCredentials: false, // Prevent CORS credential issues
 });
 
 // Image URL builder for Sanity images
+const builder = imageURLBuilder(client);
 
-//this builder is useful for our images we will be using
-const builder = imageURLBuilder(client)
-//we can now use the builder to build a url for an image
-export const urlFor = (source) => builder.image(source)
+// Export URL builder function for Sanity images
+export const urlFor = (source) => builder.image(source);
